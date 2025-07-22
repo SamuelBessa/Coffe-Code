c45dee4f0ff467f207ce750199db691a1ac8abb2
Samuel Bessa <dev.samuelbessa@gmail.com>
2025-07-22 16:40:11 -0300

    adding home and home.css

--- src/componentes/Home.jsx
+++ src/componentes/Home.jsx
@@ -1,11 +1,13 @@
+import '../styled/home.css';
+
 function Home() {
     return (
-        <div>
-            <section id="#home">
-                <div>
-                    <h3>Code com o melhor café da região</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel facilis reprehenderit sapiente veritatis quibusdam nesciunt, neque aperiam est quaerat nihil odio corrupti et nam, officiis temporibus assumenda fugit ad. Nihil.</p>
-                    <a href="#">Pegue o seu Agora</a>
+        <div className="bg-[url(./assets/img/home-img.jpg)] bg-center bg-cover text-[var(--white)]">
+            <section id="#home" className="text-[var(--white)] flex items-center min-h-screen">
+                <div className="max-w-[60rem]">
+                    <h3 className="text-[8rem] font-bold">Code com o melhor café da região</h3>
+                    <p className="text-[2rem]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel facilis reprehenderit sapiente veritatis quibusdam nesciunt, neque aperiam est quaerat nihil odio corrupti et nam, officiis temporibus assumenda fugit ad. Nihil.</p>
+                    <a href="#" className="btn">Pegue o seu Agora</a>
                 </div>
             </section>
         </div>
